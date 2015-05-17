5f03844a93e2191f993925e522f3869e7536185e
Louis-Kenzo Cahier <louiskenzo@gmail.com>
2015-05-17 23:56:35 +0200

    Demo program and makefile

---
+++ src/demo.cpp
@@ -0,0 +1,17 @@
+//––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––//
+
+#include "dye.hpp"
+#include <iostream>
+
+int main() {
+	std::cout << dye::black   << "black"   << std::endl;
+	std::cout << dye::red     << "red"     << std::endl;
+	std::cout << dye::green   << "green"   << std::endl;
+	std::cout << dye::yellow  << "yellow"  << std::endl;
+	std::cout << dye::blue    << "blue"    << std::endl;
+	std::cout << dye::magenta << "magenta" << std::endl;
+	std::cout << dye::cyan    << "cyan"    << std::endl;
+	std::cout << dye::white   << "white"   << std::endl;
+}
+
+//–––––––––––––––––––––––––––––––––––– ∎ –––––––––––––––––––––––––––––––––––––//
